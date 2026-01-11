f16f5f23144e7b524bbd0680ddbaf92b55e2b8a1
TUSHAR KUMAR <tusharkumar5090@gmail.com>
2026-01-11 19:00:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+num_1 = input("Enter  the first  number ")
+num_2 = input("Enter the second number")
+
+num1 = 100
+num2 = 200
+print("addition",num1 + num2)
+print("subtract",num1 - num2)
+print("multiplication",num1 * num2)
+print("division",num1 / num2)
+
+
---
+++ task2.py
@@ -0,0 +1,8 @@
+
+first_name = input("Enter the user first name")
+last_name = input("Enter the user last name")
+
+print("hello ",first_name +" " + last_name , "welcome to the  pycharm")
+
+
+
